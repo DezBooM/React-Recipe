fc1b2541e6e36f78971a78349d89b724b177ba66
DezBooM <dezboom3@gmail.com>
2023-01-02 18:18:14 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <h1 className="text-4xl font-bold underline bg-red-900 flex justify-center items-center">
+      Hello React with Tailwind!
+    </h1>
+      
+  )
+}
+
+export default App
